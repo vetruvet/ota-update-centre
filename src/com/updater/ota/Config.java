d551f6d197b79c508d7236cf0b78886d29bca647
Valera Trubachev <vetruvet@gmail.com>
2012-07-18 20:22:59 -0400

    use https and new domain name for urls

--- src/com/updater/ota/Config.java
+++ src/com/updater/ota/Config.java
@@ -6,8 +6,8 @@ import android.content.pm.PackageManager.NameNotFoundException;
 
 public class Config {
     public static final String GCM_SENDER_ID = "1068482628480";
-    public static final String GCM_REGISTER_URL = "http://sensation-devs.org/romupdater2/pages/regdevice.php";
-    public static final String PULL_URL = "http://sensation-devs.org/romupdater2/pages/romupdate.php";
+    public static final String GCM_REGISTER_URL = "https://www.otaupdatecenter.pro/pages/regdevice.php";
+    public static final String PULL_URL = "https://www.otaupdatecenter.pro/pages/romupdate.php";
     public static final String OTA_ID_PROP = "otaupdater.otaid";
     
     private int lastVersion = -1;
