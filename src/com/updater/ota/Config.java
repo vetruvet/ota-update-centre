48975ab71e7418a6c2e68505462a8d2529994752
Valera Trubachev <vetruvet@gmail.com>
2012-07-24 21:21:58 -0400

    add copyright notices so Config/Utils

--- src/com/updater/ota/Config.java
+++ src/com/updater/ota/Config.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 OTA Updater
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may only use this file in compliance with the license and provided you are not associated with or are in co-operation anyone by the name 'X Vanderpoel'.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.updater.ota;
 
 import java.io.File;
