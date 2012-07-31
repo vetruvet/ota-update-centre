f7fab1132c4893742633c0cdda67b8b41614daea
Valera Trubachev <vetruvet@gmail.com>
2012-07-30 20:55:32 -0400

    get rid of unused settings stuff

--- src/com/updater/ota/UpdaterSettings.java
+++ src/com/updater/ota/UpdaterSettings.java
@@ -16,23 +16,13 @@
 
 package com.updater.ota;
 
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceManager;
-import android.preference.PreferenceScreen;
 
 public class UpdaterSettings extends PreferenceActivity {
-    private SharedPreferences prefs;
-
-    private CheckBoxPreference notifPref;
-    private CheckBoxPreference ledPref;
-    private CheckBoxPreference vibratePref;
     private Preference versionPref;
-    private Preference updatePref;
 
     @Override
     @SuppressWarnings("deprecation")
@@ -40,42 +30,11 @@ public class UpdaterSettings extends PreferenceActivity {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
 
-        prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());;
-
-        notifPref = (CheckBoxPreference) findPreference("pref_notif");
-        notifPref.setChecked(prefs.getBoolean("notif", false));
-
-        ledPref = (CheckBoxPreference) findPreference("pref_led");
-        ledPref.setChecked(prefs.getBoolean("notif_led", false));
-
-        vibratePref = (CheckBoxPreference) findPreference("pref_vibrate");
-        vibratePref.setChecked(prefs.getBoolean("notif_vibrate", false));
-
         versionPref = findPreference("pref_version");
         try {
 			versionPref.setSummary(getPackageManager().getPackageInfo(getPackageName(), 0).versionName);
 		} catch (NameNotFoundException e) {
 			versionPref.setSummary(R.string.pref_version_unknown);
 		}
-
-        updatePref = findPreference("pref_update");
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        SharedPreferences.Editor editor = prefs.edit();
-
-        if (preference == notifPref) {
-            editor.putBoolean("notif", notifPref.isChecked());
-        } else if (preference == ledPref) {
-            editor.putBoolean("notif_led", ledPref.isChecked());
-        } else if (preference == vibratePref) {
-            editor.putBoolean("notif_vibrate", vibratePref.isChecked());
-        } else if (preference == updatePref) {
-            //TODO check for app updates
-        }
-
-        editor.commit();
-        return true;
     }
 }
